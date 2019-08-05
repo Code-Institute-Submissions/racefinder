66652d4e66900aa16b0c7b3157adf24ffac93cfd
Raph <raphael.p.zenou@gmail.com>
2019-08-05 09:18:15 +0100

    initial commit

---
+++ assets/apireader.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+// jQuery methods go here...
+
+
+
+
+// http://api.amp.active.com/v2/search?query=running&category=event&start_date=2013-07-04..&near=San%20Diego,CA,US&radius=50&api_key=y3ptgtcc32fd8dcakhcck2c8
+// .data-container
+
+
+});
+
+// ideas :
+// https://www.w3schools.com/howto/howto_js_autocomplete.asp
+
+
+// API documentation : 
+// http://developer.active.com/docs/read/v2_Activity_API_Search
