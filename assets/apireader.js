9f8dda41a15586b99a440f5ac728d116d990f10e
Raph <raphael.p.zenou@gmail.com>
2019-08-06 10:53:00 +0100

    starting to work on the UI : button + input

--- assets/apireader.js
+++ assets/apireader.js
@@ -6,42 +6,74 @@ $(document).ready(function(){
 console.log("Pre Parsing");
 
 
-var url ='https://cors-anywhere.herokuapp.com/http://api.amp.active.com/v2/search?query=running&category=event&start_date=2013-07-04..&near=San%20Diego,CA,US&radius=50&api_key=y3ptgtcc32fd8dcakhcck2c8';
+var urlbase ='https://cors-anywhere.herokuapp.com/http://api.amp.active.com/v2/search?query=running&category=event';
+var urlpage = '&current_page=1';
+var urlresultperpage = '&per_page=1000';
+var urlstartdate = '&start_date=2019-01-01..';
+var urlenddate = '2019-06-30';
+var urlorder = '&sort=date_asc';
+// could be 'date_desc' or 'distance'
+
+// var urlplace = '&near=San%20Diego,CA,US';§
+var urlplace = '&near=london,GB';
+
+var urlradius = '&radius=50';
+var apikey = '&api_key=y3ptgtcc32fd8dcakhcck2c8';
+
 var raceList = "<ul>";
 
+
+
+var urlcomplete = urlbase + urlpage + urlresultperpage + urlstartdate + urlenddate + urlorder + urlplace + urlradius + apikey
 
 // Using AJAX method instead of getJSON to have a bit more control 
 // https://www.youtube.com/watch?v=j-S5MBs4y0Q
 
-$.ajax ({
-	url: url,
-	dataType: 'json',
-	type: 'get',
-	cache: false,
-	success : function(data) {
-		console.log("During Parsing");
-		console.log(data);
-		$(data.results).each(function(index, value){
-			raceList += "<li> "+ value.assetName + "</li>";
+
+	$("#searchbutton").click(NewRequest());
+
+
+	function NewRequest()
+	{
+		$.ajax (
+		{
+			url: urlcomplete,
+			dataType: 'json',
+			type: 'get',
+			cache: false,
+
+			success : function(data) 
+			{
+				console.log("During Parsing");
+				console.log(data);
+				$(data.results).each(function(index, value)
+
+				{
+					raceList += "<li> "+ value.salesStartDate 
+					// + " - " + value.assetAttributes[0].attribute.attributeValue
+					+ " - " + value.assetName + "</li>";	
+				});
+					
+				console.log("Post Parsing");
+				raceList += "</ul>";
+				console.log("HTML result");
+				$('#race-data-container').append(raceList);
+				console.log("After Appending HTML");
+			}
 		});
-			
-		console.log("Post Parsing");
-		raceList += "</ul>";
-		console.log("HTML result");
-		console.log(raceList);
-		$('#race-data-container').append(raceList);
-		console.log("After Appending HTML");
 	}
+	
+
+
+
 });
-
-
-
-
 
 
 
 // http://api.amp.active.com/v2/search?query=running&category=event&start_date=2013-07-04..&near=San%20Diego,CA,US&radius=50&api_key=y3ptgtcc32fd8dcakhcck2c8
 // API key: y3ptgtcc32fd8dcakhcck2c8
+// http://developer.active.com/docs/v2_activity_api_search#ranges
+
 // CORS ERROR potential fix :
 // https://medium.com/@dtkatz/3-ways-to-fix-the-cors-error-and-how-access-control-allow-origin-works-d97d55946d9
 // https://cors-anywhere.herokuapp.com/
@@ -50,7 +82,6 @@ $.ajax ({
 
 
 
-});
 
 // ideas :
 // https://www.w3schools.com/howto/howto_js_autocomplete.asp
