aad9e2857699bd20be8118aa780a931c130ba6aa
Raph <raphael.p.zenou@gmail.com>
2019-08-12 11:39:18 +0100

    Code improved / DRY'ed using functions without atering the program's results

--- assets/apireader.js
+++ assets/apireader.js
@@ -1,28 +1,75 @@
 $(document).ready(function(){ 
 // making sure the document is fully loaded especially regarding the form
 
-	var urlbase ='https://cors-anywhere.herokuapp.com/http://api.amp.active.com/v2/search?query=running&category=event';
-
-	// url has been modified to fix a CORS Error :
-	// https://medium.com/@dtkatz/3-ways-to-fix-the-cors-error-and-how-access-control-allow-origin-works-d97d55946d9
-	// https://cors-anywhere.herokuapp.com/
-
-	var urlcomplete = "";
-
-	var number_results = 0;
-	// creating the key numbers to be displayed above the race list
+	// Initiliasing variables
+	var raceList = "<p><ul>"; 
 	var raceListNumbers ="<p>Number of results : ";
-	var raceList = "<p><ul>";
+	var number_results = 0;
+
+	
+
+
+	function URLbuilder(page)
+	{
+
+		console.log('URL builder function called');
+		// url has been modified to fix a CORS Error :
+		// https://medium.com/@dtkatz/3-ways-to-fix-the-cors-error-and-how-access-control-allow-origin-works-d97d55946d9
+		// https://cors-anywhere.herokuapp.com/
+
+		var urlcomplete = "";
+		var urlbase ='https://cors-anywhere.herokuapp.com/http://api.amp.active.com/v2/search?query=running&category=event';
+		var number_results = 0;
+
+		var pagenumber = page;
+		//defaulted to 1
+		var urlpage = '&current_page='+pagenumber;
+		var resultsperpage = $('#resultPerPage').val();
+
+		var urlresultperpage = '&per_page='+resultsperpage;
+		var numberofpages = 0;
+
+		var urlstartdate = '&start_date=2018-01-01..';
+		var urlenddate = '2019-06-30';
+		var urlorder = '&sort=date_asc'; // could be 'date_desc' or 'distance'
+		// var city = $('#inputCity').val();
+		var city = 'london';
+		var urlplace = '&near=' + city+ ',GB'; // location (city, country)
+		var urlradius = '&radius=50'; // in miles
+		var apikey = '&api_key=y3ptgtcc32fd8dcakhcck2c8';
+
+		urlcomplete = urlbase + urlpage + urlresultperpage + urlstartdate + urlenddate + urlorder + urlplace + urlradius + apikey;
+
+		console.log('Results per page:');
+		console.log(resultsperpage);
+		console.log('Page number:');
+		console.log(pagenumber);
+		console.log('URL:');
+		console.log(urlcomplete);
+
+		return [urlcomplete, resultsperpage, pagenumber];
+
+	}
+
+
+// Function to refresh the data to zero when a new request is submitted or when the page is cleared
+	function Initialise() 
+	{
+		raceList = "<p><ul>"; 
+		raceListNumbers ="<p>Number of results : ";
+		number_results = 0;
+		$('#race-data-container').html("Loading.......");
+	}
+
+
+
+
 	// Using AJAX method instead of getJSON to have a bit more control 
 	// https://www.youtube.com/watch?v=j-S5MBs4y0Q
 
 
 	function NewRequest(urlapi, resultsperpage, pagenumber)
 	{
-
-		// alert(document.getElementById('resultPerPage').value);
-
-
 
 		// doc https://api.jquery.com/jquery.ajax/
 
@@ -43,6 +90,7 @@ $(document).ready(function(){
 			success : function(data) 
 			{
 				console.log('4 - starting the API parsing');
+
 				// extraction of number of results from the API response
 				number_results = data.total_results;
 				// number of pages computed based on the above and the 'resultsperpage' var
@@ -74,8 +122,7 @@ $(document).ready(function(){
 					$('#race-data-container').append(raceList);
 					$('#race-data-container').append("<p> Page " + pagenumber + " out of " + numberofpages + "</p>");
 					
-					// $('#race-data-container').append(`"<form><br><input type="text" id="resultPerPage" value="25">
-				 //  <button class="btn" id="searchbutton">Go</button></form>`);
+					$('#race-data-container').append(`<form><br><button class="btn" id="gobutton">Go to page >></button><input type="text" id="pagenumberinput"></form>`);
 					console.log('5 - API Parsing and results populating done');
 				}
 
@@ -83,6 +130,8 @@ $(document).ready(function(){
 		});
 
 	}
+
+
 
 // clicking on the Search button
 	$("#searchbutton").click( function(e)
@@ -92,50 +141,64 @@ $(document).ready(function(){
 		// to avoid page refresh on click
 		// courtesy of https://stackoverflow.com/questions/33465557/how-to-stop-page-reload-on-button-click-jquery
 
-		console.log('1- Button clicked, now building URL');
-		// clearing the results
-		raceList = "<p><ul>"; 
-		raceListNumbers ="<p>Number of results : ";
-		number_results = 0;
-		$('#race-data-container').html('<span> Loading... </span>');
-
-
-		var pagenumber = 1;
-		var urlpage = '&current_page='+pagenumber;
-		var resultsperpage = $('#resultPerPage').val();
-		//var resultsperpage = 25;
-		var urlresultperpage = '&per_page='+resultsperpage;
-		var numberofpages = 0;
-
-		var urlstartdate = '&start_date=2018-01-01..';
-		var urlenddate = '2019-06-30';
-		var urlorder = '&sort=date_asc'; // could be 'date_desc' or 'distance'
-		// var city = $('#inputCity').val();
-		var city = 'london';
-		var urlplace = '&near=' + city+ ',GB'; // location (city, country)
-		var urlradius = '&radius=50'; // in miles
-		var apikey = '&api_key=y3ptgtcc32fd8dcakhcck2c8';
-
-		// buildingn the final url
-		var urlcomplete = urlbase + urlpage + urlresultperpage + urlstartdate + urlenddate + urlorder + urlplace + urlradius + apikey;
-
-		console.log('2.1 - Results per page input:'+ resultsperpage);
-
-
-		console.log('2.2 - URL in use after button click:');
-
-		console.log(urlcomplete);
-
-		console.log('3 - now lets launch the API request');
-
-		NewRequest(urlcomplete, resultsperpage, pagenumber );
+		Initialise();
+		//Initialising
+        var parameters = URLbuilder(1);
+        // fetching data from the URLbuilder function that uses the user input 
+		NewRequest(parameters[0], parameters[1], parameters[2]);
+		// sending the request with the correct data
 
 		return false;
 
+	});
+
+
+// clicking on the Clear button
+	$("#clearbutton").click(function(e)
+	{
+
+		e.preventDefault(); 
+		// to avoid page refresh on click
+		// courtesy of https://stackoverflow.com/questions/33465557/how-to-stop-page-reload-on-button-click-jquery
+		console.log('Clearing');
+
+		Initialise();
+		//Initialising
+
+		$('#race-data-container').html(`<span> Please enter details and hit "Search" </span>`);
+		//Giving user suggestions
+
+		return false;
+
+	});
+
+
+// clicking on the Go button
+
+
+	$("#gobutton").click(function(e)
+	{
+		e.preventDefault(); 
+		// to avoid page refresh on click
+		// courtesy of https://stackoverflow.com/questions/33465557/how-to-stop-page-reload-on-button-click-jquery
+
+		Initialise();
+		//Initialising
+
+        var parameters = URLbuilder($('#pagenumberinput').val());
+        // fetching data from the URLbuilder function that uses the user input 
+
+        //pagenumberinput"
+
+		NewRequest(parameters[0], parameters[1], parameters[2]);
+		// sending the request with the correct data
+
+		return false;
 
 	});
 
 });
+
 
 // main function called when the button is clicked
 
